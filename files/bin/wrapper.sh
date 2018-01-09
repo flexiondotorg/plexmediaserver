883c79c8a1be73df9eb217eb4c8bd4cde81417a7
Martin Wimpress <code@flexion.org>
2018-01-09 16:00:30 +0000

    Double quote all variables referencing directories. Use C.UTF-8. Refactor execution.

--- files/bin/wrapper.sh
+++ files/bin/wrapper.sh
@@ -10,8 +10,8 @@ export PLEX_MEDIA_SERVER_MAX_STACK_SIZE=3000
 export PLEX_MEDIA_SERVER_USER=root	# snaps always run using the root user
 
 # Redirect installation paths to the snap directory
-export PLEX_MEDIA_SERVER_HOME=$SNAP/usr/lib/plexmediaserver
-export LD_LIBRARY_PATH=${PLEX_MEDIA_SERVER_HOME}
+export PLEX_MEDIA_SERVER_HOME="$SNAP/usr/lib/plexmediaserver"
+export LD_LIBRARY_PATH="${PLEX_MEDIA_SERVER_HOME}":$LD_LIBRARY_PATH
 
 # Plex config directory is placed in the common directory because this can
 # contain GBs of data
@@ -21,20 +21,19 @@ export PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR="$SNAP_COMMON/Library/Applicati
 # Required to get video playback working.
 # See https://forums.plex.tv/discussion/26959/any-idea-whats-causing-this-error-when-running-start-sh
 # for details.
-export LC_ALL=C
+export LC_ALL=C.UTF-8
 
 # The tmp directory is where the mediaserver stores the transcodes
 # Place the tmp directory in the common directory
-export TMPDIR=$SNAP_COMMON/tmp
-export PLEX_MEDIA_SERVER_TMPDIR=$TMPDIR
+export TMPDIR="$SNAP_COMMON/tmp"
+export PLEX_MEDIA_SERVER_TMPDIR="$TMPDIR"
 
 # Use syslog for logging instead of sending logs to Plex Media Server.log
 export PLEX_MEDIA_SERVER_USE_SYSLOG=true
 
 # Create necessary directories
-mkdir -p $PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR
-mkdir -p $PLEX_MEDIA_SERVER_HOME
-mkdir -p $PLEX_MEDIA_SERVER_HOME/Resources
-mkdir -p $TMPDIR
+mkdir -p "$PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR"
+mkdir -p "$PLEX_MEDIA_SERVER_HOME/Resources"
+mkdir -p "$TMPDIR"
 
-$SNAP/usr/lib/plexmediaserver/Plex\ Media\ Server
+(cd "$PLEX_MEDIA_SERVER_HOME"; ./Plex\ Media\ Server)
